5608c739508a7bfbec181c542d191d113bfb0a0e
Kale-Ko <github@kaleko.dev>
2022-12-09 14:58:55 -0500

    Finish json parser

--- src/main/java/io/github/kale_ko/bjsl/parsers/JsonParser.java
+++ src/main/java/io/github/kale_ko/bjsl/parsers/JsonParser.java
@@ -3,34 +3,44 @@ package io.github.kale_ko.bjsl.parsers;
 import java.io.IOException;
 import java.io.PipedReader;
 import java.io.PipedWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.IntNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.github.kale_ko.bjsl.elements.ParsedArray;
 import io.github.kale_ko.bjsl.elements.ParsedElement;
 import io.github.kale_ko.bjsl.elements.ParsedObject;
-import io.github.kale_ko.bjsl.elements.ParsedPrimitive;
 
-public class JsonParser {
+public class JsonParser extends Parser {
     public static ParsedElement parse(String data) {
         if (data == null) {
             throw new NullPointerException("\"data\" can not be null");
         }
 
-        data = data.trim();
+        try {
+            com.fasterxml.jackson.core.JsonParser parser = JsonFactory.builder().build().createParser(data);
+            parser.setCodec(new ObjectMapper());
+            TreeNode tree = parser.readValueAsTree();
+            parser.close();
 
-        if (data.startsWith("{") && data.endsWith("}")) {
+            if (tree instanceof ObjectNode objectNode) {
+                ParsedObject objectElement = ParsedObject.create();
 
-        } else if (data.startsWith("[") && data.endsWith("]")) {
+                objectNode.fieldNames().forEachRemaining((String subKey) -> {
+                    elementify(objectElement, subKey, objectNode.get(subKey));
+                });
 
+                return objectElement;
+            } else if (tree instanceof ArrayNode) {
+
+            } else {
+                System.out.println(tree.getClass());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return null;
@@ -46,8 +56,8 @@ public class JsonParser {
                 ParsedObject objectElement = element.asJsonObject();
 
                 ObjectNode tree = JsonNodeFactory.instance.objectNode();
-                for (String subkey : objectElement.getKeys()) {
-                    nodeify(tree, subkey, objectElement.get(subkey));
+                for (String subKey : objectElement.getKeys()) {
+                    nodeify(tree, subKey, objectElement.get(subKey));
                 }
 
                 PipedWriter writer = new PipedWriter();
@@ -98,93 +108,5 @@ public class JsonParser {
         }
 
         return null;
-    }
-
-    protected static void nodeify(TreeNode tree, String key, ParsedElement element) {
-        if (element instanceof ParsedObject objectElement) {
-            ObjectNode subtree = JsonNodeFactory.instance.objectNode();
-            if (tree instanceof ObjectNode objectTree) {
-                objectTree.set(key, subtree);
-            } else if (tree instanceof ArrayNode objectTree) {
-                objectTree.add(subtree);
-            }
-
-            for (String subkey : objectElement.getKeys()) {
-                nodeify(subtree, subkey, objectElement.get(subkey));
-            }
-        } else if (element instanceof ParsedArray arrayElement) {
-            ArrayNode subtree = JsonNodeFactory.instance.arrayNode();
-            if (tree instanceof ObjectNode objectTree) {
-                objectTree.set(key, subtree);
-            } else if (tree instanceof ArrayNode objectTree) {
-                objectTree.add(subtree);
-            }
-
-            for (ParsedElement subElement : arrayElement.getValues()) {
-                nodeify(subtree, key, subElement);
-            }
-        } else if (element instanceof ParsedPrimitive primitiveElement) {
-            if (primitiveElement.isString()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asString());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asString());
-                }
-            } else if (primitiveElement.isByte()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asByte());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asByte());
-                }
-            } else if (primitiveElement.isChar()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asChar());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asChar());
-                }
-            } else if (primitiveElement.isShort()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asShort());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asShort());
-                }
-            } else if (primitiveElement.isInteger()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asInteger());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asInteger());
-                }
-            } else if (primitiveElement.isLong()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asLong());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asLong());
-                }
-            } else if (primitiveElement.isFloat()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asFloat());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asFloat());
-                }
-            } else if (primitiveElement.isDouble()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asDouble());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asDouble());
-                }
-            } else if (primitiveElement.isBoolean()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.put(key, primitiveElement.asBoolean());
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.add(primitiveElement.asBoolean());
-                }
-            } else if (primitiveElement.isNull()) {
-                if (tree instanceof ObjectNode objectTree) {
-                    objectTree.putNull(key);
-                } else if (tree instanceof ArrayNode objectTree) {
-                    objectTree.addNull();
-                }
-            }
-        }
     }
 }
---
+++ src/main/java/io/github/kale_ko/bjsl/parsers/Parser.java
@@ -0,0 +1,183 @@
+package io.github.kale_ko.bjsl.parsers;
+
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.DoubleNode;
+import com.fasterxml.jackson.databind.node.FloatNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.LongNode;
+import com.fasterxml.jackson.databind.node.NullNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ShortNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import io.github.kale_ko.bjsl.elements.ParsedArray;
+import io.github.kale_ko.bjsl.elements.ParsedElement;
+import io.github.kale_ko.bjsl.elements.ParsedObject;
+import io.github.kale_ko.bjsl.elements.ParsedPrimitive;
+
+public abstract class Parser {
+    protected static void elementify(ParsedElement element, String key, JsonNode node) {
+        if (node instanceof ObjectNode objectNode) {
+            ParsedObject subElement = ParsedObject.create();
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, subElement);
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(subElement);
+            }
+
+            objectNode.fieldNames().forEachRemaining((String subKey) -> {
+                elementify(subElement, subKey, objectNode.get(subKey));
+            });
+        } else if (node instanceof ArrayNode arrayNode) {
+            ParsedArray subElement = ParsedArray.create();
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, subElement);
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(subElement);
+            }
+
+            arrayNode.elements().forEachRemaining((JsonNode subNode) -> {
+                elementify(subElement, key, subNode);
+            });
+        } else if (node instanceof TextNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromString(node.asText()));
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromString(node.asText()));
+            }
+        } else if (node instanceof ShortNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromShort((short) node.asInt()));
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromShort((short) node.asInt()));
+            }
+        } else if (node instanceof IntNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromInteger(node.asInt()));
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromInteger(node.asInt()));
+            }
+        } else if (node instanceof LongNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromLong(node.asLong()));
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromLong(node.asLong()));
+            }
+        } else if (node instanceof FloatNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromFloat((float) node.asDouble()));
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromFloat((float) node.asDouble()));
+            }
+        } else if (node instanceof DoubleNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromDouble(node.asDouble()));
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromDouble(node.asDouble()));
+            }
+        } else if (node instanceof BooleanNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromBoolean(node.asBoolean()));
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromBoolean(node.asBoolean()));
+            }
+        } else if (node instanceof NullNode) {
+            if (element instanceof ParsedObject objectElement) {
+                objectElement.set(key, ParsedPrimitive.fromNull());
+            } else if (element instanceof ParsedArray arrayElement) {
+                arrayElement.add(ParsedPrimitive.fromNull());
+            }
+        }
+    }
+
+    protected static void nodeify(TreeNode node, String key, ParsedElement element) {
+        if (element instanceof ParsedObject objectElement) {
+            ObjectNode subNode = JsonNodeFactory.instance.objectNode();
+            if (node instanceof ObjectNode objectNode) {
+                objectNode.set(key, subNode);
+            } else if (node instanceof ArrayNode arrayNode) {
+                arrayNode.add(subNode);
+            }
+
+            for (String subkey : objectElement.getKeys()) {
+                nodeify(subNode, subkey, objectElement.get(subkey));
+            }
+        } else if (element instanceof ParsedArray arrayElement) {
+            ArrayNode subNode = JsonNodeFactory.instance.arrayNode();
+            if (node instanceof ObjectNode objectNode) {
+                objectNode.set(key, subNode);
+            } else if (node instanceof ArrayNode objectNode) {
+                objectNode.add(subNode);
+            }
+
+            for (ParsedElement subElement : arrayElement.getValues()) {
+                nodeify(subNode, key, subElement);
+            }
+        } else if (element instanceof ParsedPrimitive primitiveElement) {
+            if (primitiveElement.isString()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asString());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asString());
+                }
+            } else if (primitiveElement.isByte()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asByte());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asByte());
+                }
+            } else if (primitiveElement.isChar()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asChar());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asChar());
+                }
+            } else if (primitiveElement.isShort()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asShort());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asShort());
+                }
+            } else if (primitiveElement.isInteger()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asInteger());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asInteger());
+                }
+            } else if (primitiveElement.isLong()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asLong());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asLong());
+                }
+            } else if (primitiveElement.isFloat()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asFloat());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asFloat());
+                }
+            } else if (primitiveElement.isDouble()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asDouble());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asDouble());
+                }
+            } else if (primitiveElement.isBoolean()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.put(key, primitiveElement.asBoolean());
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.add(primitiveElement.asBoolean());
+                }
+            } else if (primitiveElement.isNull()) {
+                if (node instanceof ObjectNode objectNode) {
+                    objectNode.putNull(key);
+                } else if (node instanceof ArrayNode objectNode) {
+                    objectNode.addNull();
+                }
+            }
+        }
+    }
+}
