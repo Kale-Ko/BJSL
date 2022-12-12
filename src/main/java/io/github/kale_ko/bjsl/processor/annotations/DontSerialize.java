3eff21607ca2daea6b2bce5e05061e8f324288f2
Kale-Ko <github@kaleko.dev>
2022-12-12 01:04:54 -0500

    Object processor toElement

---
+++ src/main/java/io/github/kale_ko/bjsl/processor/annotations/DontSerialize.java
@@ -0,0 +1,9 @@
+package io.github.kale_ko.bjsl.processor.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@java.lang.annotation.Target({ ElementType.FIELD, ElementType.METHOD })
+@java.lang.annotation.Retention(RetentionPolicy.RUNTIME)
+@java.lang.annotation.Inherited()
+public @interface DontSerialize {}
