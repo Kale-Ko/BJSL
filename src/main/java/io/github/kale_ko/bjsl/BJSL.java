666e24c83bc2a86fef05b8084cd308ed7f03484b
Kale-Ko <github@kaleko.ga>
2022-12-15 16:19:51 -0500

    Add nonstatic instance and remove methods using internal types

--- src/main/java/io/github/kale_ko/bjsl/BJSL.java
+++ src/main/java/io/github/kale_ko/bjsl/BJSL.java
@@ -1,8 +1,8 @@
 package io.github.kale_ko.bjsl;
 
-import io.github.kale_ko.bjsl.elements.ParsedElement;
 import io.github.kale_ko.bjsl.parsers.BinaryParser;
 import io.github.kale_ko.bjsl.parsers.JsonParser;
+import io.github.kale_ko.bjsl.parsers.Parser;
 import io.github.kale_ko.bjsl.parsers.PropertiesParser;
 import io.github.kale_ko.bjsl.parsers.TomlParser;
 import io.github.kale_ko.bjsl.parsers.YamlParser;
@@ -17,65 +17,59 @@ public class BJSL {
     private static PropertiesParser propertiesParser = new PropertiesParser();
     private static BinaryParser binaryParser = new BinaryParser();
 
-    private BJSL() {}
+    protected Parser parser;
+    protected ObjectProcessor processor;
 
-    public static ParsedElement parseJson(String data) {
-        return jsonParser.toElement(data);
+    public BJSL(Parser parser, ObjectProcessor processor) {
+        this.parser = parser;
+        this.processor = processor;
+    }
+
+    public <T> T parse(String data, Class<T> clazz) {
+        return this.processor.toObject(this.parser.toElement(data), clazz);
+    }
+
+    public String stringify(Object object) {
+        return this.parser.toString(this.processor.toElement(object));
     }
 
     public static <T> T parseJson(String data, Class<T> clazz) {
         return objectProcessor.toObject(jsonParser.toElement(data), clazz);
     }
 
-    public static String stringifyJson(ParsedElement element) {
-        return jsonParser.toString(element);
-    }
-
-    public static ParsedElement parseYaml(String data) {
-        return yamlParser.toElement(data);
+    public static String stringifyJson(Object object) {
+        return jsonParser.toString(objectProcessor.toElement(object));
     }
 
     public static <T> T parseYaml(String data, Class<T> clazz) {
         return objectProcessor.toObject(yamlParser.toElement(data), clazz);
     }
 
-    public static String stringifyYaml(ParsedElement element) {
-        return yamlParser.toString(element);
-    }
-
-    public static ParsedElement parseToml(String data) {
-        return tomlParser.toElement(data);
+    public static String stringifyYaml(Object object) {
+        return yamlParser.toString(objectProcessor.toElement(object));
     }
 
     public static <T> T parseToml(String data, Class<T> clazz) {
         return objectProcessor.toObject(tomlParser.toElement(data), clazz);
     }
 
-    public static String stringifyToml(ParsedElement element) {
-        return tomlParser.toString(element);
-    }
-
-    public static ParsedElement parseProperties(String data) {
-        return propertiesParser.toElement(data);
+    public static String stringifyToml(Object object) {
+        return tomlParser.toString(objectProcessor.toElement(object));
     }
 
     public static <T> T parseProperties(String data, Class<T> clazz) {
         return objectProcessor.toObject(propertiesParser.toElement(data), clazz);
     }
 
-    public static String stringifyProperties(ParsedElement element) {
-        return propertiesParser.toString(element);
-    }
-
-    public static ParsedElement parseBinary(String data) {
-        return binaryParser.toElement(data);
+    public static String stringifyProperties(Object object) {
+        return propertiesParser.toString(objectProcessor.toElement(object));
     }
 
     public static <T> T parseBinary(String data, Class<T> clazz) {
         return objectProcessor.toObject(binaryParser.toElement(data), clazz);
     }
 
-    public static String stringifyBinary(ParsedElement element) {
-        return binaryParser.toString(element);
+    public static String stringifyBinary(Object object) {
+        return binaryParser.toString(objectProcessor.toElement(object));
     }
 }
