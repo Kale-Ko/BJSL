564b35f3ae1db3b007ced68558982e35f704c760
Kale-Ko <github@kaleko.dev>
2023-01-14 21:36:11 -0500

    Use builders instead of constructors

--- src/main/java/io/github/kale_ko/bjsl/parsers/SmileParser.java
+++ src/main/java/io/github/kale_ko/bjsl/parsers/SmileParser.java
@@ -1,9 +1,29 @@
 package io.github.kale_ko.bjsl.parsers;
 
+import com.fasterxml.jackson.core.PrettyPrinter;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.StreamWriteFeature;
 import com.fasterxml.jackson.dataformat.smile.SmileFactory;
+import com.fasterxml.jackson.dataformat.smile.SmileFactoryBuilder;
+import com.fasterxml.jackson.dataformat.smile.databind.SmileMapper;
 
 public class SmileParser extends Parser {
-    public SmileParser() {
-        super(SmileFactory.builder().build(), null);
+    protected SmileParser(SmileFactory factory, SmileMapper mapper, PrettyPrinter prettyPrinter) {
+        super(factory, mapper, prettyPrinter);
+    }
+
+    public static class Builder {
+        public Builder() {}
+
+        public SmileParser build() {
+            SmileFactoryBuilder factoryBuilder = (SmileFactoryBuilder) SmileFactory.builder();
+            factoryBuilder.configure(StreamReadFeature.USE_FAST_DOUBLE_PARSER, true);
+            factoryBuilder.configure(StreamWriteFeature.USE_FAST_DOUBLE_WRITER, true);
+            factoryBuilder.configure(StreamWriteFeature.WRITE_BIGDECIMAL_AS_PLAIN, true);
+
+            SmileFactory factory = factoryBuilder.build();
+
+            return new SmileParser(factory, new SmileMapper(factory), null);
+        }
     }
 }
