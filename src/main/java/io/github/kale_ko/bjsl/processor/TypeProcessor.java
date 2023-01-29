0a33cbbfae6f15fc2e426e8f84cb57c17825857b
Kale-Ko <github@kaleko.dev>
2023-01-29 13:38:12 -0500

    Document everything and add build/publish

--- src/main/java/io/github/kale_ko/bjsl/processor/TypeProcessor.java
+++ src/main/java/io/github/kale_ko/bjsl/processor/TypeProcessor.java
@@ -2,8 +2,30 @@ package io.github.kale_ko.bjsl.processor;
 
 import io.github.kale_ko.bjsl.elements.ParsedElement;
 
+/**
+ * A type processor for converting elements to objects and objects to elements
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public interface TypeProcessor {
+    /**
+     * Converts an Object into a {@link ParsedElement}
+     *
+     * @param object
+     *        The object to convert
+     * @return A new {@link ParsedElement} representing the Object
+     * @since 1.0.0
+     */
     public ParsedElement toElement(Object object);
 
+    /**
+     * Converts a {@link ParsedElement} into an Object
+     *
+     * @param element
+     *        The element to convert
+     * @return A new Object representing the element
+     * @since 1.0.0
+     */
     public Object toObject(ParsedElement element);
 }
