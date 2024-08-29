02fad42c2e708bcb1cc529e74c43927175bb624f
Kale-Ko <github@kaleko.dev>
2024-08-28 20:45:08 -0400

    Remove usage of Unsafe

--- src/main/java/io/github/kale_ko/bjsl/processor/reflection/InitializationUtil.java
+++ src/main/java/io/github/kale_ko/bjsl/processor/reflection/InitializationUtil.java
@@ -5,18 +5,16 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import sun.misc.Unsafe;
 
 /**
  * A utility class for initializing objects and arrays from a class type
  * <p>
  * Objects can be initialized using 0-args constructors or using {@link sun.misc.Unsafe}
  *
- * @version 1.7.0
+ * @version 1.11.1
  * @since 1.7.0
  */
 @SuppressWarnings("unchecked")
@@ -51,39 +49,7 @@ public class InitializationUtil {
     }
 
     /**
-     * Initialize a class unsafely using {@link sun.misc.Unsafe}
-     * <p>
-     * Will attempt to use {@link #initialize(Class)} before resorting to {@link sun.misc.Unsafe}
-     *
-     * @param clazz The class to initialize
-     * @param <T>   The type to initialize
-     *
-     * @return The initialized instance of clazz
-     */
-    public static <T> @Nullable T initializeUnsafe(@NotNull Class<T> clazz) {
-        T attempt = initialize(clazz);
-        if (attempt != null) {
-            return attempt;
-        }
-
-        try {
-            Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
-            unsafeField.setAccessible(true);
-            Unsafe unsafe = (Unsafe) unsafeField.get(null);
-            return (T) unsafe.allocateInstance(clazz);
-        } catch (InstantiationException | IllegalAccessException | NoSuchFieldException e) {
-            if (BJSL.getLogger() != null) {
-                StringWriter writer = new StringWriter();
-                new RuntimeException("Nonfatal error while parsing:", e).printStackTrace(new PrintWriter(writer));
-                BJSL.getLogger().warning(writer.toString());
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Initialize a class safely using {@link Array#newInstance(Class, int)}
+     * Initialize an array safely using {@link Array#newInstance(Class, int)}
      *
      * @param clazz  The class to initialize
      * @param length The length of the new array
